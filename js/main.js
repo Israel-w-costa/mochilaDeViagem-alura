57fabf8d2905849af0fdfee54b2311933f7bb1bf
israel <israelwendellc@gmail.com>
2023-07-18 20:33:23 -0300

    adicioando dados ao localStorage

--- js/main.js
+++ js/main.js
@@ -1,33 +1,56 @@
 const form = document.getElementById ("novoItem")
 const lista = document.getElementById ("lista")
-const itens = []
+const itens = JSON.parse(localStorage.getItem("itens") ) || []
+
+itens.forEach((elemento) => {
+    CriaElemento (elemento)
+    
+});
 
 form.addEventListener ('submit', (evento) => {
     evento.preventDefault ()
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
-    CriaElemento (nome.value, quantidade.value)
+    const existe = itens.find( elemento => elemento.nome === nome.value )
+
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade" : quantidade.value
+    } 
+   
+    console.log (existe)
+
+    if (existe) {
+        itemAtual.id = existe.id
+        atualizaElementos (itemAtual)
+    } else {
+        itemAtual.id = itens.length
+
+        CriaElemento (itemAtual)
+        itens.push (itemAtual)
+    }
+
+    localStorage.setItem ("itens", JSON.stringify(itens))
+
+
     nome.value = ""
     quantidade.value =""
 } )
 
-function CriaElemento (nome, quantidade) {
+function CriaElemento (item) {
     const novoItem = document.createElement('li')
     novoItem.classList.add("item")
-
     const adicionandoEstilo = document.createElement ('strong')
-    adicionandoEstilo.innerHTML = quantidade
+    adicionandoEstilo.innerHTML = item.quantidade
+    adicionandoEstilo.dataset.id = item.id
     novoItem.appendChild(adicionandoEstilo)
-    novoItem.innerHTML += nome
+    novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
 
-    const itemAtual = {
-        "nome": nome,
-        "quantidade" : quantidade
-    }
-    itens.push (itemAtual)
-    localStorage.setItem ("item", JSON.stringify(itens))
-
 }
 
+
+function atualizaElementos (item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
