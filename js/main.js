ec456831997c8eb826b6cac3b91083ac72fe2903
israel <israelwendellc@gmail.com>
2023-07-19 19:04:18 -0300

    removendo itens dinamicamente da mochila

--- js/main.js
+++ js/main.js
@@ -18,13 +18,13 @@ form.addEventListener ('submit', (evento) => {
         "quantidade" : quantidade.value
     } 
    
-    console.log (existe)
 
     if (existe) {
         itemAtual.id = existe.id
         atualizaElementos (itemAtual)
+        itens [itens.findIndex (elemento => elemento.id === existe.id)] = itemAtual
     } else {
-        itemAtual.id = itens.length
+        itemAtual.id = itens[itens.lenght -1] ? itens [itens.lenght -1]+1 : 0
 
         CriaElemento (itemAtual)
         itens.push (itemAtual)
@@ -44,8 +44,11 @@ function CriaElemento (item) {
     adicionandoEstilo.innerHTML = item.quantidade
     adicionandoEstilo.dataset.id = item.id
     novoItem.appendChild(adicionandoEstilo)
+    
     novoItem.innerHTML += item.nome
-
+    
+    novoItem.appendChild(botaoDeleta(item.id))
+    
     lista.appendChild(novoItem)
 
 }
@@ -53,4 +56,23 @@ function CriaElemento (item) {
 
 function atualizaElementos (item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta (id) {
+    const elementobotao = document.createElement ('button')
+    elementobotao.innerText = "x"
+   
+    elementobotao.addEventListener ("click", function (tag) {
+        deletebotao (this.parentNode, id)
+    })
+
+    return elementobotao
+}
+
+function deletebotao (tag, id) {
+    tag.remove()
+
+    itens.splice ( itens.findIndex (elemento => elemento.id === id) ,1)
+     
+    localStorage.setItem ("itens", JSON.stringify(itens))
 }
